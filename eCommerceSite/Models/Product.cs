e0230dff5b084ec82b1db054a24de3b0d0ba2f87
Dillon Stricklett <dillonstricklett@live.com>
2020-07-25 22:15:54 -0700

    Created product model

---
+++ eCommerceSite/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace eCommerceSite.Models
+{
+    /// <summary>
+    /// A salable product.
+    /// </summary>
+    public class Product
+    {
+        [Key] // Makes sure that this property is a primary key
+        public int ProductId { get; set; }
+        /// <summary>
+        /// The consumer facing name of the product.
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// The retail price as US currency.
+        /// </summary>
+        public double Price { get; set; }
+        /// <summary>
+        /// Category product falls under. Ex: Gadgets or Furnature
+        /// </summary>
+        public string Category { get; set; }
+    }
+}
