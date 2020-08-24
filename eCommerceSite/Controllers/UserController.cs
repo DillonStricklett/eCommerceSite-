98ebeb4500dfa14dca4d5d26b8fcbe13d57a44f8
Dillon Stricklett <dillonstricklett@live.com>
2020-08-23 22:22:30 -0700

    Added log in error message for invalid credentials

--- eCommerceSite/Controllers/UserController.cs
+++ eCommerceSite/Controllers/UserController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using eCommerceSite.Data;
 using eCommerceSite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace eCommerceSite.Controllers
 {
@@ -49,6 +50,39 @@ namespace eCommerceSite.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            //UserAccount account =
+            //    await (from u in _context.UserAccounts
+            //           where (u.Username == model.UsernameOrEmail ||
+            //           u.Email == model.UsernameOrEmail) &&
+            //           u.Password == model.Password
+            //           select u).SingleOrDefaultAsync(); same code, different syntax below
+            UserAccount account = await
+                _context.UserAccounts
+                    .Where(userAcc => (userAcc.Username == model.UsernameOrEmail ||
+                                        userAcc.Email == model.UsernameOrEmail) &&
+                                        userAcc.Password == model.Password)
+                    .SingleOrDefaultAsync();
+            if(account == null)
+            {
+                // Credentials did not match
+
+                // Custom Error message
+                ModelState.AddModelError(string.Empty, "Credentials were not found.");
+
+                return View(model);
+            }
+            // Log user into website
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
