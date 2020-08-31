c94cb49c303a58fb66cd38da504a3020f3751441
Dillon Stricklett <dillonstricklett@live.com>
2020-08-30 21:57:01 -0700

    Issue#26 shopping cart (#37)
    * Added shopping cart button link to navigation and add to cart button for each product.
    
    * Refactored get single product DB query
    
    * A single product can now be added to cart
    
    * Able to store multiple items in shopping cart
    
    * Created basic summary page(does not work with empty cookie)
    
    * Refactored cart controller
    
    * Added a message for an empty cart on the summary page

---
+++ eCommerceSite/Controllers/CartController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace eCommerceSite.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ProductContext _context;
+        private readonly IHttpContextAccessor _httpContext;
+
+        public CartController(ProductContext context, IHttpContextAccessor httpContext)
+        {
+            _context = context;
+            _httpContext = httpContext;
+        }
+
+        /// <summary>
+        /// Adds a product to the shopping cart
+        /// </summary>
+        /// <param name="id">The id of the product to add</param>
+        /// <returns></returns>
+        public async Task<IActionResult> Add(int id) // Id of the product to add
+        {
+            // Get product from the DB
+            Product p = await ProductDb.GetProductByIdAsync(_context, id);
+
+            CookieHelper.AddProductToCart(p, _httpContext);
+           
+            // Redirect back to previous page
+            return RedirectToAction("Index", "Product");
+        }
+
+        public IActionResult Summary()
+        {
+            return View(CookieHelper.GetCartProducts(_httpContext));
+        }
+    }
+}
